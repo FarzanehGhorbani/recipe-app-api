354d8911e6259380ccba7c946e4cf3f3f4052055
farghorbani <50452602+FarzanehGhorbani@users.noreply.github.com>
2022-04-23 17:46:30 +0430

    Added recipe model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -53,3 +53,13 @@ class TestModels(TestCase):
             name='Cucomber'
         )
         assert str(ingredient)==ingredient.name
+
+    def test_recipe_str(self):
+        """Test the recipe string representation"""
+        recipe=models.Recipe.objects.create(
+            user=sample_user(),
+            title='Steak and mushroom sauce',
+            time_minutes=5,
+            price=5.00
+        )
+        assert str(recipe)==recipe.title
